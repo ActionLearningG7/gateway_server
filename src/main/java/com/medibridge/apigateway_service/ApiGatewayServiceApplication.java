bdacad11dcc3c79bdbeb18bd84d3cde5cf9be77a
omprakashsaggam <omprakash.saggam@gmail.com>
2026-01-03 22:37:28 +0100

    auth, cros centralization

--- src/main/java/com/medibridge/apigateway_service/ApiGatewayServiceApplication.java
+++ src/main/java/com/medibridge/apigateway_service/ApiGatewayServiceApplication.java
@@ -2,9 +2,10 @@ package com.medibridge.apigateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
